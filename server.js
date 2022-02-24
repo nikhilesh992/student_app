b74960aed7e40aa6aa6ae8fa4dfa103ef60f6195
Nikhilesh Pattanayak <pattnaiknikhilesh@gmail.com>
2022-02-24 17:47:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ fastify.post('/update', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 5000)
+    await fastify.listen(process.env.PORT || 80)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
